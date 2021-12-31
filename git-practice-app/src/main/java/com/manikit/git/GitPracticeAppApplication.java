4e996a1eb59395cc6f1427b897d76271f538065b
manikdahiphale2016 <manik.dahiphale2016@gmail.com>
2021-12-31 13:37:54 +0530

    Initial commit/Project created

---
+++ git-practice-app/src/main/java/com/manikit/git/GitPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.manikit.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeAppApplication.class, args);
+	}
+
+}
---
+++ git-practice-app/src/test/java/com/manikit/git/GitPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikit.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
